34f0febcf38250fa922df0e4f9bf38ae6c4fb7f7
ValerioMik <valerio.avallone@hotmail.com>
2021-11-24 16:12:03 +0100

    primo passaggio

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaDer/>
+   
+
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaDer from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaDer
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+<header>
+    <div class="logo">
+         <img class="logo" alt="Vue logo" src="../assets\img\dc-logo.png">
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+        </ul>
+    </nav>
+   
+</header>
+</template>
+
+<script>
+export default {
+  name: 'HeaDer',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header{
+    display: flex;
+    li{
+        display: inline;
+        margin: 0px 10px;
+    } 
+    ul{
+        align-items: center;
+
+    }
+    .logo{
+         align-items: center;
+    }
+}
+
+</style>
