203275e6e094baf4076aa89335c76c22d835d4aa
ValerioMik <valerio.avallone@hotmail.com>
2021-11-25 15:38:04 +0100

    tutto bloccato

---
+++ src/components/Contenuticard.vue
@@ -0,0 +1,23 @@
+<template>
+<section>
+    <div class="thumbs">
+
+    </div>
+    <div class="title">
+        
+    </div>
+    
+</section>
+ 
+</template>
+
+<script>
+export default {
+  name: "Contenuticard",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
